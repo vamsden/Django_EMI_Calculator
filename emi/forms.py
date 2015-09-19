552eb27111d93e21f8aa0aa325b120022573d239
Emil George James <emilgeorgejames@gmail.com>
2015-09-20 00:52:07 +0530

    Delete forms.py

--- emi/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms 
-from django.forms import ModelForm
-
-class emiforms(forms.ModelForm):
-	loanAmount = forms.IntegerField()
-	intrate = forms.IntegerField()
-	period = forms.IntegerField()
-
